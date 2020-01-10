9f7222c7221741be23ee286d755fa4255f8bc829
NTN <n@gmail.com>
2020-01-10 20:41:34 +0700

    Expanding the information of this object

--- feature.js
+++ feature.js
@@ -5,3 +5,18 @@ if (i > 0)
 	console.log("Hello");
 else
 	console.log("Hi");
+
+function user(name, age, address)
+{
+	name : "name";
+	age : "age";
+	address: "address";
+}
+
+user1.prototype.noisinh=function()
+{
+	return this.address;
+}
+var user1 = new user("user1",18, "Can Tho");
+var user2 = new user("user2", 21, "Thanh Hoa");
+
